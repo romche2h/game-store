a82c0eef93bb36ffa50eec7314979154cc491df3
romche2h <rommche98@gmail.com>
2025-03-14 14:44:47 +0100

    new project start

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+import './styles/_variables.scss';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <div className='sidebar'>sidebar</div>
+        <div className='header_left'>header-left</div>
+        <div className='header_right'>header-right</div>
+        <div className='about'>about</div>
+        <div className='place_img'>place-img</div>
+        <div className='place_text'>place-text</div>
+        <div className='platform'>platform</div>
+        <div className='gallery'>gallery</div>
+        <div className='footer'>footer</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import sassDts from 'vite-plugin-sass-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    sassDts({
+      quietDeps: true,
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        quietDeps: true,
+      },
+    },
+  },
+});
